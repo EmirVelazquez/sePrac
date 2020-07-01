1778ef9baccb35cd5671f8bd947ad8976890d646
EmirVelazquez <emirleovelazquez@gmail.com>
2020-06-30 23:24:12 -0500

    changed script so selenium can now hit the enter key to execute the search, also max window size. Ran script through the terminal.

--- sel_prac.py
+++ sel_prac.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
 
 # In Case you need to use different web drivers, it must be specified which browsers
 # to use. Note: you alse need to install the webdriver for each browser.
@@ -8,9 +9,14 @@ driver = webdriver.Chrome()
 # driver = webdriver.Ie()
 
 
-driver.set_page_load_timeout(10)
+driver.set_page_load_timeout(5)
+driver.maximize_window()
 driver.get("https://www.google.com/")
 driver.find_element_by_name("q").send_keys("100 page machine learning book")
-driver.find_element_by_name("btnK").click()
+driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
 time.sleep(4)
+
+print("Test Ran Successfully")
+
+# Close the window and stop the test here
 driver.quit()
