c6be6d71cd6295145ac4a9e2216f0f55ac73b426
EmirVelazquez <emirleovelazquez@gmail.com>
2020-07-06 22:19:03 -0500

    had the driver take the current url and placed it in a variable to print in terminal.

--- sel_prac.py
+++ sel_prac.py
@@ -16,6 +16,9 @@ driver.find_element_by_name("q").send_keys("100 page machine learning book")
 driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
 time.sleep(4)
 
+# Here we can place the current url and store it in a variable to be used later
+browser_url = driver.current_url
+print(browser_url)
 print("Test Ran Successfully")
 
 # Close the window and stop the test here
